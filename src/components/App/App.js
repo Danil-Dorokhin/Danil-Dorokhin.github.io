e11e703f24761635f13da67df719f400c54a063e
Dorokhin <dorokhin.d.danil@gmail.com>
2020-12-05 19:21:08 +0600

    initial_commit

---
+++ src/components/App/App.js
@@ -0,0 +1,30 @@
+import React from "react";
+
+import "./App.css";
+
+const App = () => {
+    return (
+        <div className="Container">
+            <p>RUS/ENG</p>+
+            <p>----------------------</p>
+            <img src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse2.mm.bing.net%2Fth%3Fid%3DOIP.A_RtQRl3ALKzklz-D1zCsAHaEK%26pid%3DApi&f=1" />
+            <p>Dorokhin Danil</p>+
+            <p>----------------------</p>
+            <p>[Git][Vk]</p>
+            <p>----------------------</p>
+            <p>About me</p>
+            <p>----------------------</p>
+            <p>Grid with my skills</p>+
+            <p>----------------------</p>
+            <p>My Projects</p>
+            <p>----------------------</p>
+            <p>My Experience</p>
+            <p>----------------------</p>
+            <p>Education</p>
+            <p>----------------------</p>
+            <p>Contacts</p>
+        </div>
+    );
+};
+
+export default App;
---
+++ src/components/data/index.js
@@ -0,0 +1,83 @@
+const myData = {
+    myName: { eng: "Danil Dorokhin", ru: "Данил Дорохин" },
+    //
+    aboutMeTitle: {
+        eng: "Обо мне",
+        ru: "About me",
+    },
+    aboutMe: {
+        eng:
+            "Information about me, here im going to tell something about what kind of person i am and what are my goals",
+        ru: "Информация о том, кто я такой и какими личными качествами обладаю",
+    },
+    //
+    skillsTitle: {
+        eng: "My skills",
+        ru: "Мои навыки",
+    },
+    skills: [
+        "JavaScript (ES6)",
+        "React",
+        "React-Native",
+        "Redux",
+        "HTML",
+        "CSS",
+        "Python",
+        "Linux",
+    ],
+    //
+    projectsTitle: {
+        eng: "My works",
+        ru: "Мои работы",
+    },
+    projects: {
+        ClassicalMusic: {
+            name: "Classical Music",
+            image:
+                "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse2.mm.bing.net%2Fth%3Fid%3DOIP.A_RtQRl3ALKzklz-D1zCsAHaEK%26pid%3DApi&f=1",
+            description: {
+                eng:
+                    "React-native classical music player for iOS and Android. It was developed during my internship in devsteam.mobi",
+                ru:
+                    "Приложение для прослушивания классической музыки, разработанное на React-native для iOS и Android. Оно было разработано во время стажировки в devsteam.mobi",
+            },
+            url: "https://apps.apple.com/us/app/id1532389989#?platform=iphone",
+        },
+    },
+    //
+    myExperienceTitle: {
+        eng: "My experience",
+        ru: "Мой опыт",
+    },
+    myExperience: [
+        {
+            eng: {
+                companyName: "Devsteam.mobi",
+                companyDescription: "React-native development company",
+                myRole: "Frontend developer intern",
+                whatAchieved: [1, 2, 3, 4, 5],
+                time: ["June 2020", "November 2020"],
+            },
+            ru: {
+                companyName: "Devsteam.mobi",
+                companyDescription:
+                    "Компания, специализирующаяся на разработке мобильных приложений на React-native",
+                myRole: "Стажер frontend разработчик",
+                whatAchieved: [1, 2, 3, 4, 5],
+                time: ["Июнь 2020", "Ноябрь 2020"],
+            },
+        },
+    ],
+    //
+    myContactsTitle: {
+        eng: "Contacts",
+        ru: "Контактная информация",
+    },
+    myContacts: {
+        phone: "+7 705 464 20 89",
+        telegram: "+7 996 938 09 54",
+        github: "https://github.com/Danil-Dorokhin",
+        gmail: "dorokhin.d.danil@gmail.com",
+    },
+};
+export default myData;
