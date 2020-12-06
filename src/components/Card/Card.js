76a5f7bac19bf4eb4d87db2811036f6ac1c30243
Dorokhin <dorokhin.d.danil@gmail.com>
2020-12-07 00:16:18 +0600

    telegram_ref_added

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -13,12 +13,17 @@ const Card = ({ lang }) => {
             <p style={{ width: 500, fontSize: 18 }} className="mb-0">
                 {myData.aboutMe[lang]}
             </p>
-            <a href="https://github.com/Danil-Dorokhin">
-                <i className="fa fa-github "></i>
-            </a>
-            <a href="https://vk.com/hatheg_kla">
-                <i className="fa fa-vk "></i>
-            </a>
+            <div className='btn-container'>
+                <a href="https://github.com/Danil-Dorokhin">
+                    <i className="fa fa-github "></i>
+                </a>
+                <a href="https://vk.com/hatheg_kla">
+                    <i className="fa fa-vk "></i>
+                </a>
+                <a href="https://t.me/dorokhinD">
+                    <i className="fa fa-telegram"></i>
+                </a>
+            </div>
         </div>
     );
 };
